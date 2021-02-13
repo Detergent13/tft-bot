b4cb6bd6d4bf24454eaaa7935cca41e563b18d6d
Some Bad Scripts <64361203+BadMaliciousPyScripts@users.noreply.github.com>
2021-02-13 20:36:51 +0100

    Replaced the some functions.
    Replaced queuing with auto accepting, tft lobby creation, play again and changed the surrendering function.
    I am mainly using LCU in my changes.

--- tft.py
+++ tft.py
@@ -7,143 +7,261 @@ pkg_resources.require("PyAutoGUI==0.9.50")
 pkg_resources.require("opencv-python==4.2.0.34")
 pkg_resources.require("python-imageseach-drov0==1.0.6")
 
+import requests
+import base64
+import os
 import pyautogui as auto
 from python_imagesearch.imagesearch import imagesearch as search
 import time
 from printy import printy
 from printy import inputy
+import urllib3
 
-
+urllib3.disable_warnings()
+# If u havent installed your game in default path(windows) set your path here
+PATH = "default"
 auto.FAILSAFE = False
 
+class xtra:
+    def base64encode(text):
+        text = base64.b64encode(text.encode("ascii")).decode("ascii")
+        return text
+
+class lcu:
+    def connect(lockfile_path="default"):
+        if lockfile_path == "default":
+            lockfile_path = "C:\\Riot Games\\League of Legends\\lockfile"
+        if lcu.check_exist(lockfile_path):
+            return lcu.readFile(lockfile_path)
+        else:
+            raise Exception("Couldn't read lockfile!\nThis could mean that either the \
+path is not the right or the League Client is not opened!")
+
+    def check_exist(lockfile_path):
+        return os.path.exists(lockfile_path)
+
+    def readFile(lockfile_path):
+        lockfile = open(lockfile_path, "r")
+        data = lockfile.read().split(":")
+        data_dict = {
+            "port": data[2],
+            "url": "https://127.0.0.1:{}".format(data[2]),
+            "auth": "riot:{}".format(data[3]),
+            "connection_method": data[4]
+        }
+        return data_dict
+
+    def start_search(lcu_data):
+        auth = xtra.base64encode(lcu_data["auth"])
+        headers = {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+            "Authorization": F"Basic {auth}"
+        }
+        url = lcu_data["url"] + "/lol-lobby/v2/lobby/matchmaking/search"
+        request = requests.post(url, headers=headers, verify=False)
+        return request.status_code
+
+    def play_again(lcu_data):
+        auth = xtra.base64encode(lcu_data["auth"])
+        headers = {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+            "Authorization": F"Basic {auth}"
+        }
+        url = lcu_data["url"] + "/lol-lobby/v2/play-again"
+        request = requests.post(url, headers=headers, verify=False)
+        return request.status_code
+
+    def create_game_lobby_tft(lcu_data, gm:str="Normal"):
+        if gm.upper() != "NORMAL" and gm.upper() != "RANKED":
+            raise ValueError("Defined gamemode is unvalid!\nValid gamemode are Normal and Ranked.")
+        else:
+            if gm.upper() == "NORMAL":
+                q_id = 1090
+            if gm.upper() == "RANKED":
+                q_id = 1100
+        auth = xtra.base64encode(lcu_data["auth"])
+        headers = {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+            "Authorization": F"Basic {auth}"
+        }
+        data = '{{"queueId":{}}}'.format(q_id)
+        url = lcu_data["url"] + "/lol-lobby/v2/lobby/"
+        request = requests.post(url, headers=headers, data=data, verify=False)
+        return request.status_code
+
+    def create_tutorial_lobby_tft(lcu_data):
+        auth = xtra.base64encode(lcu_data["auth"])
+        headers = {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+            "Authorization": F"Basic {auth}"
+        }
+        data = '{"queueId":1110}'
+        url = lcu_data["url"] + "/lol-lobby/v2/lobby/"
+        request = requests.post(url, headers=headers, data=data, verify=False)
+        return request.json()
+
+    def get_current_summoner_in_queue(lcu_data, rc=False):
+        auth = xtra.base64encode(lcu_data["auth"])
+        headers = {
+            "Accept": "application/json",
+            "Authorization": F"Basic {auth}"
+        }
+        url = lcu_data["url"] + "/lol-matchmaking/v1/ready-check"
+        request = requests.get(url, headers=headers, verify=False)
+        request_json = request.json()
+        if "state" in request_json:
+            if request_json["state"] == "Invalid":
+                return True
+            elif request_json["state"] == "InProgress":
+                if rc == True:
+                    return 2
+                return True
+        elif request_json["httpStatus"] == 404:
+            return False
+
+    def get_current_summoner_ready_check(lcu_data, rc=False):
+        is_in_q = lcu.get_current_summoner_in_queue(lcu_data,True)
+        if is_in_q == 2:
+            return True
+        if is_in_q == True and is_in_q != 2 and rc == True:
+            return 2
+        return False
+
+    def auto_accept_current_ready_check(lcu_data):
+        ready_check = lcu.get_current_summoner_ready_check(lcu_data,True)
+        if ready_check == True:
+            auth = xtra.base64encode(lcu_data["auth"])
+            headers = {
+                "Content-Type": "application/json",
+                "Accept": "application/json",
+                "Authorization": F"Basic {auth}"
+            }
+            url = lcu_data["url"] + "/lol-matchmaking/v1/ready-check/accept"
+            requests.post(url, headers=headers, verify=False)
+            return "Accepted!"
+        elif ready_check == 2:
+            return "Waiting for ready check..."
+        else:
+            return "Not in queue!"
 
 # Start utility methods
-def onscreen(path, precision=0.8):
-    return search(path, precision)[0] != -1
+class auto_gui:
+    def onscreen(path, precision=0.8):
+        return search(path, precision)[0] != -1
+
+    def search_to(path):
+        pos = search(path)
+        if auto_gui.onscreen(path):
+            auto.moveTo(pos)
+            return pos
+
+    def click_key(key, delay=.1):
+        auto.keyDown(key)
+        time.sleep(delay)
+        auto.keyUp(key)
 
 
-def search_to(path):
-    pos = search(path)
-    if onscreen(path):
-        auto.moveTo(pos)
-        # print(path + " found")
-        return pos
-#   else:
-    #   print(path + " not found")
+    def click_left(delay=.1):
+        auto.mouseDown()
+        time.sleep(delay)
+        auto.mouseUp()
 
 
-def click_key(key, delay=.1):
-    auto.keyDown(key)
-    time.sleep(delay)
-    auto.keyUp(key)
+    def click_right(delay=.1):
+        auto.mouseDown(button='right')
+        time.sleep(delay)
+        auto.mouseUp(button='right')
 
 
-def click_left(delay=.1):
-    auto.mouseDown()
-    time.sleep(delay)
-    auto.mouseUp()
-
-
-def click_right(delay=.1):
-    auto.mouseDown(button='right')
-    time.sleep(delay)
-    auto.mouseUp(button='right')
-
-
-def click_to(path, delay=.1):
-    if onscreen(path):
-        auto.moveTo(search(path))
-        click_left(delay)
+    def click_to(path, delay=.1):
+        if auto_gui.onscreen(path):
+            auto.moveTo(search(path))
+            auto_gui.click_left(delay)
         # print(path + " clicked")
 # End utility methods
 
 
 # Start main process
-def queue():
-    if onscreen("./captures/tft logo.png"):
-        click_to("./captures/find match ready.png")
-    while not onscreen("./captures/loading.png"):
-        time.sleep(1)
-        click_to("./captures/accept.png")
-
-    print("Loading!")
-    loading()
-
-
-def loading():
-    while not onscreen("./captures/1-1.png"):
-        time.sleep(1)
-
-    print("Match starting!")
-    start()
+class main:
+    def queue():
+        lcu_data = lcu.connect(PATH)
+        print("Creating tft lobby...")
+        lcu.create_game_lobby_tft(lcu_data)
+        lcu.start_search(lcu_data)
+        while True:
+            response = lcu.auto_accept_current_ready_check(lcu_data)
+            if response == "Accepted!":
+                break
+        main.loading()
 
 
-def start():
-    while onscreen("./captures/1-1.png"):
-        auto.moveTo(888, 376)
-        click_right()
-
-    print("In the match now!")
-    main()
-
-
-def buy(iterations):
-    for i in range(iterations):
-        click_to("./captures/diana.png")
-        click_to("./captures/fiora.png")
-        click_to("./captures/yasuo.png")
-        click_to("./captures/garen.png")
-        click_to("./captures/wukong.png")
-        click_to("./captures/nidalee.png")
-
-
-def main():
-    while not onscreen("./captures/2-4.png"):
-        buy(1)
-        time.sleep(1)
-    while onscreen("./captures/2-4.png"):
-        auto.moveTo(928, 396)
-        click_right()
-        time.sleep(0.25)
-
-    time.sleep(5)
-
-    if onscreen("./captures/2-5.png"):
-        while not onscreen("./captures/3-2.png"): # change this if you want to surrender at a different stage
-            buy(1)
-            click_to("./captures/reroll.png")
+    def loading():
+        while not auto_gui.onscreen("./captures/1-1.png"):
             time.sleep(1)
-    if onscreen("./captures/3-2.png"): # (and this)
-        print("Surrendering now!")
-        surrender()
+        print("Match starting!")
+        main.start()
 
 
-def surrender():
-    click_to("./captures/settings.png")
+    def start():
+        while auto_gui.onscreen("./captures/1-1.png"):
+            auto.moveTo(888, 376)
+            auto_gui.click_right()
+        print("In the match now!")
+        main.main()
 
-    while not onscreen("./captures/surrender 1.png"):
+
+    def buy(iterations):
+        for i in range(iterations):
+            auto_gui.click_to("./captures/diana.png")
+            auto_gui.click_to("./captures/fiora.png")
+            auto_gui.click_to("./captures/yasuo.png")
+            auto_gui.click_to("./captures/garen.png")
+            auto_gui.click_to("./captures/wukong.png")
+            auto_gui.click_to("./captures/nidalee.png")
+
+
+    def main():
+        while not auto_gui.onscreen("./captures/2-4.png"):
+            main.buy(1)
+            time.sleep(1)
+        while auto_gui.onscreen("./captures/2-4.png"):
+            auto.moveTo(928, 396)
+            auto_gui.click_right()
+            time.sleep(0.25)
+        time.sleep(5)
+
+        if auto_gui.onscreen("./captures/2-5.png"):
+            while not auto_gui.onscreen("./captures/3-2.png"): # change this if you want to surrender at a different stage
+                main.buy(1)
+                auto_gui.click_to("./captures/reroll.png")
+                time.sleep(1)
+        if auto_gui.onscreen("./captures/3-2.png"): # (and this)
+            print("Surrendering now!")
+            main.surrender()
+
+
+    def surrender():
+        auto.press('enter')
+        auto.write('/ff')
+        auto.press('enter')
+        while not auto_gui.onscreen("./captures/surrender 2.png"):
+            auto.press('enter')
+            auto.write('/ff')
+            auto.press('enter')
         time.sleep(1)
-    while not onscreen("./captures/surrender 2.png"):
-        click_to("./captures/surrender 1.png")
-
-    time.sleep(1)
-    click_to("./captures/surrender 2.png")
-    time.sleep(15)
-
-    time.sleep(1)
-
-    while onscreen("./captures/missions ok.png"):
-        click_to("./captures/missions ok.png")
-    time.sleep(5)
-    while onscreen("./captures/skip waiting for stats.png"):
-        click_to("./captures/skip waiting for stats.png")
-    time.sleep(5)
-    while onscreen("./captures/play again.png"):
-        click_to("./captures/play again.png")
-
-    time.sleep(10)
-    print("Queuing up again!")
-    queue()
+        auto_gui.click_to("./captures/surrender 2.png")
+        time.sleep(15)
+        time.sleep(1)
+        lcu_data = lcu.connect(PATH)
+        lcu.play_again(lcu_data)
+        time.sleep(10)
+        input()
+        print("Queuing up again!")
+        main.queue()
 # End main process
 
 
@@ -161,8 +279,7 @@ printy(r"""
 """)
 
 printy(f"Welcome! You're running Detergent's TFT bot.\nPlease feel free to ask questions or contribute at https://github.com/Detergent13/tft-bot", "nB")
-auto.alert("Press OK when you're in a TFT lobby!\n")
 print("Bot started, queuing up!")
-queue()
+main.queue()
 
 # End auth + main script
