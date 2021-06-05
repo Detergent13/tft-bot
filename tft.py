bee9b26d3f7d0bb01828352e52552c86993a5c3d
Z Xian <zhaxxdev@gmail.com>
2021-06-04 20:08:24 -0700

    Added checks for wins, reconnects (#24)
    * ldakf
    
    * changed round
    
    * Replace duplicated code w/ new method
    
    Also fixed a bit of grammar
    
    Co-authored-by: Detergent <50428606+Detergent13@users.noreply.github.com>

--- tft.py
+++ tft.py
@@ -109,7 +109,13 @@ def buy_item():
         auto.moveTo(734,974)
         click_left()
     
-
+def checks(): #added checks to see if game was inturrupted 
+    if onscreen("./captures/play again.png"):
+        won_match()
+    if onscreen("./captures/reconnect.png"):
+        print("reconnecting!")
+        time.sleep(0.5)
+        click_to("./captures/reconnect.png")
 
 def main():
     while not onscreen("./captures/2-4.png"):
@@ -117,24 +123,45 @@ def main():
         #new feature  IT WORKS AAAAAAA
         buy_item()
         time.sleep(1)
+        checks() #added checks to see if game was inturrupted 
     while onscreen("./captures/2-4.png"):
         auto.moveTo(928, 396)
         click_right()
         time.sleep(0.25)
 
     time.sleep(5)
-
+    
     if onscreen("./captures/2-5.png"):
         while not onscreen("./captures/3-4.png"): # change this if you want to surrender at a different stage
             buy(1)
             buy_item()
             click_to("./captures/reroll.png")
             time.sleep(1)
+            checks() #added checks to see if game was inturrupted 
+            
     if onscreen("./captures/3-4.png"): # (and this)
         print("Surrendering now!")
         surrender()
 
+def end_match():
+    while onscreen("./captures/missions ok.png"):
+        click_to("./captures/missions ok.png")
+        time.sleep(2)
+    while onscreen("./captures/skip waiting for stats.png"):
+        click_to("./captures/skip waiting for stats.png")
+    time.sleep(5)
+    while onscreen("./captures/play again.png"):
+        click_to("./captures/play again.png")
 
+def won_match(): #Added wonmatch case
+    print("Looks like we won! Re-queuing")
+    time.sleep(3)
+
+    end_match()
+
+    time.sleep(5)
+    queue()
+    
 def surrender():
     click_to("./captures/settings.png")
 
@@ -145,20 +172,13 @@ def surrender():
 
     time.sleep(1)
     click_to("./captures/surrender 2.png")
-    time.sleep(15)
+    time.sleep(10)
 
     time.sleep(1)
 
-    while onscreen("./captures/missions ok.png"):
-        click_to("./captures/missions ok.png")
-        time.sleep(2)
-    while onscreen("./captures/skip waiting for stats.png"):
-        click_to("./captures/skip waiting for stats.png")
-    time.sleep(5)
-    while onscreen("./captures/play again.png"):
-        click_to("./captures/play again.png")
+    end_match()
 
-    time.sleep(10)
+    time.sleep(5)
     print("Queuing up again!")
     queue()
 # End main process
